9716849552127c365369f2217d0325966d108d4f
Shivish468 <shivamaryan468@gmail.com>
2024-01-25 00:35:35 +0530

    Properties of String

---
+++ Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+     public static void main(String[] args) {
+        String name = "Shivam Kumar Ishwar";
+        System.out.println(name.length());
+        System.out.println(name.charAt(7));
+        System.out.println(name.substring(13,19));
+        System.out.println(name.indexOf('K'));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.replace('m', 'a'));
+     }
+}
