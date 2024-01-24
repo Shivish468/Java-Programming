e667db116afd1ec462a09d1d26212409525231ab
Shivish468 <shivamaryan468@gmail.com>
2024-01-25 04:38:28 +0530

    Program for gcd

--- GCD.java
+++ GCD.java
@@ -15,6 +15,7 @@ public class GCD {
     // public static void main(String[] args) {
     //     System.out.println(gcd(8,12));
     // }
+    // method 2 TC is optimized
     static int gcd(int a, int b) {
         if (b == 0) {
             return a;
