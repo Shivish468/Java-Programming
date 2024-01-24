052a1c8f75f049593304e99c4e2423569bce4ae0
Shivish468 <shivamaryan468@gmail.com>
2024-01-25 05:28:37 +0530

    Program for prime number

---
+++ Prime.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Prime {
+    // for small n it's okay but not good tc for long n
+//     static boolean isPrime(int n) {
+//         if (n <= 1) return false;
+//         for (int i=2; i*i<=n; i++) {
+//             if (n%i == 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a number: ");
+//         int n = sc.nextInt();
+//         System.out.println((isPrime(n)) ? "Yes, It's a prime number" : "No, It's not a prime number");
+//         sc.close();
+//     }
+// for long n with optimized method
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n==2 || n==3) return true;
+        if (n%2 == 0 || n%3 == 0) {
+            return false;
+        }
+        for (int i=5; i*i<=n; i+=6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number (-1 to quit): ");
+        while (true) {
+            int n = sc.nextInt();
+            if (n == -1) break;
+            System.out.println((isPrime(n)) ? "Yes, It's a prime number" : "Not a prime number! Enter again: ");
+        }
+        sc.close();
+    }
+}
