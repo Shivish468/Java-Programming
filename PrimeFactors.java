7f6d3cf2f18c57e52ed1e5d6867dfa69a9c351d1
Shivish468 <shivamaryan468@gmail.com>
2024-01-25 15:26:07 +0530

    Program for prime factors of a number

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -25,44 +25,44 @@ public class PrimeFactors {
     // }
 
     // method 2
-    // static void primeFactors(int n) {
-    //     if (n <= 1) return;
-    //     for (int i=2; i*i<=n; i++) {
-    //         while (n%i == 0 ) {
-    //             System.out.print(i+ " ");
-    //             n = n / i;
-    //         }
-    //     }
-    //     if (n > 1) {
-    //         System.out.println(n);
-    //     }
-    // }
-
-    // method 3
     static void primeFactors(int n) {
         if (n <= 1) return;
-        while (n % 2 == 0) {
-            System.out.print(2 +" ");
-            n /= 2;
-        }
-        while (n % 3 == 0) {
-            System.out.print(3 +" ");
-            n /= 3;
-        }
-        for (int i=5; i*i<=n; i+=6) {
-            while (n % i == 0) {
-                System.out.print(i +" ");
-                n /= i;
-            }
-            while (n % (i+2) == 0) {
-                System.out.print((i+2) +" ");
-                n /= (i+2);
+        for (int i=2; i*i<=n; i++) {
+            while (n%i == 0 ) {
+                System.out.print(i+ " ");
+                n = n / i;
             }
         }
-        if (n > 3) {
-            System.out.println(3);
+        if (n > 1) {
+            System.out.println(n);
         }
     }
+
+    // method 3
+    // static void primeFactors(int n) {
+    //     if (n <= 1) return;
+    //     while (n % 2 == 0) {
+    //         System.out.print(2 +" ");
+    //         n /= 2;
+    //     }
+    //     while (n % 3 == 0) {
+    //         System.out.print(3 +" ");
+    //         n /= 3;
+    //     }
+    //     for (int i=5; i*i<=n; i+=6) {
+    //         while (n % i == 0) {
+    //             System.out.print(i +" ");
+    //             n /= i;
+    //         }
+    //         while (n % (i+2) == 0) {
+    //             System.out.print((i+2) +" ");
+    //             n /= (i+2);
+    //         }
+    //     }
+    //     if (n > 3) {
+    //         System.out.println(3);
+    //     }
+    // }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
