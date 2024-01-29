c7a5ba27080954ba6ab0c28efed6999c9352d17a
Shivish468 <shivamaryan468@gmail.com>
2024-01-29 23:39:19 +0530

    Patttern 2 created

--- Pattern.java
+++ Pattern.java
@@ -5,12 +5,24 @@ public class Pattern {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=i; j++) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
-        }
+
+        // Pattern 1
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        // Pattern 2
+        // for(int i=n; i>=1; i--) {
+        //     for(int j=i; j>=1; j--) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+        // Pattern 3
         sc.close();
     }
 }
